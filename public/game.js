d6d5e9e5fab78b39511381ee883143ac7d99ce35
Ashray Arora <ashrayarora833@gmail.com>
2022-04-06 19:36:47 +0530

    Final Game

--- public/game.js
+++ public/game.js
@@ -148,7 +148,7 @@ fetch('http://localhost:8000/getCourseById?course=624c78e2a62f475c8b6a60ee').the
     return data.json();
 }).then((completedata)=>{
 
-    console.log(completedata.data.levels[0].bginfolist[0].bgtext)
+    // console.log(completedata.data.levels[0].bginfolist[0].bgtext)
 
     for (let i = 0; i <completedata.data.levels[0].bginfolist.length ; i++) {
         let data1= `
@@ -161,10 +161,24 @@ fetch('http://localhost:8000/getCourseById?course=624c78e2a62f475c8b6a60ee').the
                        <button type="button" class="btn btn-success completedBtn">Complete</button></center>
                        
                 </div>`;
+        let data2=`
+ <div class="col-md-4">
+                <div class="card1" style="width: 18rem;">
+                    <img src="https://th.bing.com/th/id/OIP.pKuNnk8zv8bVJ_9qoviMZQHaE8?pid=ImgDet&rs=1" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Course Name ${completedata.data.courseName}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Level Number ${completedata.data.levels[0].levelnumber}</h6>
+                        </div>
+                        <p class="card-text">Welcome to the Course</p>
+                        <button  class="btn btn-danger startbutton" id="startBtn">Start Course</button>
+                    </div>
+                </div>
+            </div>
+         </div>`
 
 
         let elementId = "box"+(i+1);
-
+        document.getElementById("startCourse").innerHTML=data2;
         if(document.getElementById(elementId) == null){
             console.log("not found")
         }
@@ -174,12 +188,6 @@ fetch('http://localhost:8000/getCourseById?course=624c78e2a62f475c8b6a60ee').the
         }
 
     }
-    // if(document.getElementById('completedBtn').onclick === true)
-    // {
-    //     console.log("btnClicked");
-    //     score+=10;
-    //     document.getElementById('cPercent').innerHTML=score;
-    // }
 
     let completedbtns = document.getElementsByClassName('completedBtn');
 
@@ -199,5 +207,26 @@ fetch('http://localhost:8000/getCourseById?course=624c78e2a62f475c8b6a60ee').the
             }
         }
     }
-})
+    let startbutton = document.getElementById("startBtn").onclick = () =>{
+        console.log("Start Button Clicked");
+        startbutton.disabled=true;
+        document.getElementById("startCourse").style.visibility='hidden'
 
+        let element = document.getElementById("scroll");
+        element.style.visibility='visible';
+    }
+
+})
+//
+
+
+
+
+
+// const button = document.getElementById('startCourse');
+// console.log(button); // 👉️ button#btn
+//
+// // ✅ Works
+// button.onclick = function click() {
+//     console.log('Button clicked');
+// };
